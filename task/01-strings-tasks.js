253a769e0929117f93c18455799421935a97d8ac
kruglikovAlex <kruglikov_alex@tut.by>
2018-02-21 02:47:35 +0300

    origin master

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -55,7 +55,8 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return `Hello, ${firstName} ${lastName}!`;
+
 }
 
 /**
